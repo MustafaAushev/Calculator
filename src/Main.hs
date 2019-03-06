8779cd0b7f2f06f8176abf26529e82d182349f6e
MusAushev <mustafa-aushev@mail.ru>
2019-03-06 14:11:03 +0300

    change

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,39 @@ module Main where
 
 import Calculator
 
+import System.Environment
+
 main :: IO()
 main = do 
+    args <- getArgs
+    parseArgs args
+    
+parseArgs :: [String] -> IO()
+parseArgs []        = calculating
+parseArgs y@(x:_)   = do
+    case x of
+         "--help"       -> usage
+         "--version"    -> version
+         _              -> calculating
+         
+calculating :: IO()
+calculating = do
+    putStrLn $ "Введите выражение для подсчёта"
     str <- getLine
+    putStr str
+    putStr "="
     print ( calculator str )
+
+usage :: IO()
+usage = putStrLn usageStr
+
+usageStr :: String
+usageStr =  concat[ "Использование:\n"                                                  ,
+                    "   Введите любое выражение, кроме \"--help\" и \"--version\", \n"     ,
+                    "   и калькулятор посчитает его     .\n"           ]
+    
+version :: IO()
+version = putStrLn versionStr
+
+versionStr :: String
+versionStr =    "Calculator, version 1.1."
